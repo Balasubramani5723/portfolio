46c5698ee6340663cf1b198aa179654473b284bb
Balasubramani5723 <139948340+Balasubramani5723@users.noreply.github.com>
2025-03-30 17:37:39 +0530

    Add files via upload

---
+++ Portfilio/script.js
@@ -0,0 +1,7 @@
+/* ========================== typing animation =========================== */
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Gamer"],
+    typeSpeed:100,
+    Background:60,
+    loop:true
+})
